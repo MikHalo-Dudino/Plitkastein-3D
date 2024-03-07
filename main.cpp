6a8767c010216b800c8a42a9d7fc4f501af89f3f
Michael Dudkin <134552025+MikHalo-Dudino@users.noreply.github.com>
2024-03-07 21:10:12 +0300

    Update main.cpp потолок

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include <SFML\Graphics.hpp>
 #include <vector>
 #include <iostream>
-using namespace sf;
-#define WIN_WEIGHT 1280 + 400
-#define WIN_EIGHT 720
+#include <fstream>
+//minimap settings
 #define WALL_SIZE 20
-#define P_SIZE 10
+#define WALL_COLOR sf::Color::Magenta
+#define PLAYER_SIZE 10
+#define PLAYER_COLOR sf::Color::Red
+//window settings
+#define WIN_WIDTH 1280
+#define WIN_HEIGHT 720
+#define MIN_WALL_HEIGHT 100
+#define MAP_SIZE 400
+const int MAX_DIST = (MAP_SIZE) * 1.41;
+//camera settings
+#define RAY_STEP 1
+#define FOV_RANGE pi/2
+#define MOVE_DIST 5
+#define ROTATE_ANGLE 0.05
+//player settings
+#define MAX_PLAYER_HP 100;
+//other
 #define pi 3.1415
-#define WALL_COLOR Color::Magenta
 
 enum Direction
 {
@@ -15,33 +29,63 @@ enum Direction
 };
 
 typedef std::vector<std::string> maptype;
+
+class GameObject
+{
+public:
+	virtual std::pair<float, float> getSize();
+	virtual std::pair<float, float> getCoord();			// WIP
+	virtual sf::Color getColor();
+	virtual void objectEvent();
+
+};
 class Player
+{
+private:
+	std::pair<float, float> coord;
+	int health_point;
+public:
+	Player(float x, float y)
+	{
+		coord = { x, y };
+		health_point = MAX_PLAYER_HP;
+	}
+	std::pair<float, float> getCoord()
+	{													// WIP
+		return coord;
+	}
+	int getHP()
+	{
+		return health_point;
+	}
+	void setCoord(float x, float y)
+	{
+		coord = { x, y };
+	}
+	void setHP(int value)
+	{
+		health_point = value;
+	}
+};
+class Camera
 {
 private:
 	std::pair<float, float> coord;
 	float angle;
 public:
-	Player(float x, float y)
+	Camera(float x, float y)
 	{
-		coord = {x, y};
+		coord = { x, y };
 		angle = 0;
 	}
 
-	Player(float x, float y, float new_angle) : Player(x, y)
+	Camera(float x, float y, float new_angle) : Camera(x, y)
 	{
 		angle = new_angle;
 	}
-
-	void setCoord(std::pair<float, float> new_coord)
-	{
-		coord = new_coord;
-	}
-
 	void move(Direction direction, int distance)
 	{
-		/*float distance = 5;*/
 		float dx, dy;
-
 		switch (direction)
 		{
 		case Direction::Forward:
@@ -50,7 +94,7 @@ public:
 			break;
 		case Direction::Left:
 			dx = distance * sin(angle);
-			dy = -distance* cos(angle);
+			dy = -distance * cos(angle);
 			break;
 		case Direction::Right:
 			dx = -distance * sin(angle);
@@ -67,151 +111,140 @@ public:
 		coord.second += dy;
 	}
 
-	void setAngle(float new_angle)
-	{
-		angle = new_angle;
-	}
-
 	void rotate(float d_angle)
 	{
 		angle += d_angle;
 	}
 
-	void draw(RenderWindow& window)
+	void draw(sf::RenderWindow& window)
 	{
-		CircleShape playerShape(P_SIZE);
-		playerShape.setOrigin(Vector2f(P_SIZE, P_SIZE));
-		playerShape.setFillColor(Color::Red);
-		playerShape.setPosition(Vector2f(coord.first, coord.second));
+		sf::CircleShape playerShape(PLAYER_SIZE / 2);
+		playerShape.setOrigin(sf::Vector2f(PLAYER_SIZE / 2, PLAYER_SIZE / 2));
+		playerShape.setFillColor(PLAYER_COLOR);
+		playerShape.setPosition(sf::Vector2f(coord.first, coord.second));
 		window.draw(playerShape);
 	}
 
-	float drawRay(RenderWindow& window, maptype map, float angle)
+	float getDist(maptype map, float angle)
 	{
 		float c = 0;
-		float x;
-		float y;
-		for (; c < 500; c += 0.5) 
-		{
+		float x, y;		//end of ray coords
+		for (; c < MAP_SIZE; c += RAY_STEP) {
 			x = coord.first + c * cos(angle);
 			y = coord.second + c * sin(angle);
 			if (map[round(y / WALL_SIZE)][round(x / WALL_SIZE)] != ' ') break;
-			
-		} 
-		//Vertex ray[] = {Vector2f(coord.first , coord.second), Vector2f(x, y)};
+
+		}
+		//Vertex ray[] = {sf::Vector2f(coord.first , coord.second), sf::Vector2f(x, y)};
 		//window.draw(ray, 2, Lines);
 		return pow(pow((x - coord.first), 2) + pow((y - coord.second), 2), 0.5);
 	}
 
-	RectangleShape distToWall(int dist, int index)
+	sf::RectangleShape distToWall(int dist, int index)
 	{
-		float height;
-		int alpha;
-		if (dist < 15) 
+		float wall_height;
+		if (dist < 22)
 		{
-			height = WIN_EIGHT;
-		
+			wall_height = WIN_HEIGHT;
 		}
 		else
 		{
-			height = 100 * WIN_EIGHT / dist;
+			wall_height = MIN_WALL_HEIGHT * MAP_SIZE / dist;
 		}
-		RectangleShape object(Vector2f(1, height));
-		object.setFillColor(Color(255, 255, 255, height/8));
-		object.setPosition(index, WIN_EIGHT / 2 - height / 2);
-		return object;
+		sf::RectangleShape wall_object(sf::Vector2f(1, wall_height));
+		wall_object.setFillColor(sf::Color(0.14 * wall_height, 0.14 * wall_height, 0.14 * wall_height));
+		wall_object.setOrigin(0, wall_height / 2);
+		wall_object.setPosition(index, WIN_HEIGHT / 2);
+		return wall_object;
 	}
-
-	void drawFov(RenderWindow &window, RenderWindow& window_game, maptype map)
+	sf::RectangleShape ceillingDraw() 
 	{
-		int n = 1280;
+		sf::RectangleShape ceiling_object(sf::Vector2f(WIN_WIDTH, WIN_HEIGHT / 2));
+		ceiling_object.setFillColor(sf::Color::Blue);
+		return ceiling_object;
+	}
+	sf::RectangleShape floorDraw()
+	{
+		sf::RectangleShape floor_object(sf::Vector2f(WIN_WIDTH, WIN_HEIGHT / 2));
+		floor_object.setPosition(0, WIN_HEIGHT / 2);
+		floor_object.setFillColor(sf::Color(255, 255, 0));
+		return floor_object;
+	}
+	void drawFov(sf::RenderWindow& window, sf::RenderWindow& window_game, maptype map)
+	{
+		int n = WIN_WIDTH;
 		int index = 0;
-		for (float i = -pi/4; i < pi/4; i += pi/(2 * n))
+		sf::RectangleShape ceiling = ceillingDraw();
+		sf::RectangleShape floor = floorDraw();
+		window_game.draw(ceiling);
+		window_game.draw(floor);
+		for (float i = -FOV_RANGE / 2; i < FOV_RANGE / 2; i += FOV_RANGE / n)
 		{
-			float dist = drawRay(window, map, angle + i);
-			RectangleShape line = distToWall(dist, index);
-			window_game.draw(line);
+			
+			float dist = getDist(map, angle + i);
+			sf::RectangleShape wall_line = distToWall(dist, index);
+			
+			window_game.draw(wall_line);
 			index++;
 		}
-
 	}
 };
-
-void drawMap(RenderWindow& win, maptype map)
+void drawMap(sf::RenderWindow& win, maptype map)
 {
-	RectangleShape wall;
+	sf::RectangleShape wall;
 	wall.setFillColor(WALL_COLOR);
 	wall.setOrigin(WALL_SIZE / 2, WALL_SIZE / 2);
-	wall.setSize(Vector2f(WALL_SIZE, WALL_SIZE));
+	wall.setSize(sf::Vector2f(WALL_SIZE, WALL_SIZE));
 	for (int i = 0; i < map.size(); i++)
 		for (int j = 0; j < map[0].size(); j++)
 		{
 			if (map[i][j] == '1')
 			{
-				wall.setPosition(Vector2f(j * WALL_SIZE, i * WALL_SIZE));
+				wall.setPosition(sf::Vector2f(j * WALL_SIZE, i * WALL_SIZE));
 				win.draw(wall);
 			}
 		}
-
 }
-
-
-
-void key_pressed(sf::Event& event, Player &player) {
+void key_pressed(sf::Event& event, Camera& player) {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-		player.move(Forward, 5);
+		player.move(Forward, MOVE_DIST);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-		player.move(Left, 5);
+		player.move(Left, MOVE_DIST);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-		player.move(Back, 5);
+		player.move(Back, MOVE_DIST);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-		player.move(Right, 5);
+		player.move(Right, MOVE_DIST);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-		player.rotate(-0.05);
+		player.rotate(-ROTATE_ANGLE);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-		player.rotate(0.05);
+		player.rotate(ROTATE_ANGLE);
 	}
 }
 
-void mouse_move(sf::Event& event, Player& player, float& mouse_x)
+maptype loadMap(std::string filename)
 {
-	float dx = event.mouseMove.x - mouse_x;
-	player.rotate(dx / (500 * pi));
-	
+	std::ifstream filein(filename);
+	maptype map;
+	while (!filein.eof())
+	{
+		std::string buff;
+		std::getline(filein, buff);
+		map.push_back(buff);
+	}
+	return map;
 }
-
 
 int main()
 {
-	std::vector<std::string> map;
-	map.push_back("11111111111111111111");
-	map.push_back("1            1     1");
-	map.push_back("11           1     1");
-	map.push_back("1            1     1");
-	map.push_back("1            1     1");
-	map.push_back("1            1     1");
-	map.push_back("1            1     1");
-	map.push_back("11   1111111111  11");
-	map.push_back("1    1             1");
-	map.push_back("1    1             1");
-	map.push_back("1             1    1");
-	map.push_back("1                  1");
-	map.push_back("1                  1");
-	map.push_back("1       111        1");
-	map.push_back("1                  1");
-	map.push_back("1                  1");
-	map.push_back("1        11111     1");
-	map.push_back("1                  1");
-	map.push_back("11111111111111111111");
-	
-	RenderWindow window(VideoMode(400, 400), "minimap");
-	RenderWindow window_game(VideoMode( 1280, 720), "game");
-	Player player(100, 60, 1);
+	maptype map = loadMap("map.txt");
+	sf::RenderWindow window(sf::VideoMode(MAP_SIZE, MAP_SIZE), "minimap");
+	sf::RenderWindow window_game(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "game");
+	Camera player(100, 300, 1);
 
 	while (window_game.isOpen()) {
 		drawMap(window, map);
@@ -220,18 +253,15 @@ int main()
 		window.display();
 		window_game.display();
 		sf::Event event;
-		
+
 		while (window_game.pollEvent(event)) {
 			switch (event.type) {
-			case sf::Event::Closed: // закрытие окна
+			case sf::Event::Closed:
 				window_game.close();
 				break;
 
-			case sf::Event::KeyPressed: // нажата клавиша клавиатуры
+			case sf::Event::KeyPressed: 
 				key_pressed(event, player);
-				break;
-
-			case sf::Event::MouseButtonPressed: // нажата кнопка мыши
 				break;
 
 			default:
